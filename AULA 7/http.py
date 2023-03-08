f3406d1287860c1d0081a2b7f36ec716450856b3
Manu <emanuellybs14@gmail.com>
2023-03-08 11:29:04 -0300

    aa

--- AULA 7/http.py
+++ AULA 7/http.py
@@ -7,35 +7,117 @@ Created on Tue Mar  7 08:36:38 2023
 
 import requests 
 import json 
+import pyodbc
+import time
+import matplotlib.pyplot as plt
+import pyodbc
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
 
 proxies = {'https':"https://disrct:etsds10243110@10.224.200.26:8080"}
 r = requests.get('https://chat.openai.com/chat', proxies=proxies)
 
-# url = 'https://random.dog/woof.json'
+
+#Pegar dados Firebase -------------------------------------------------------------------------------------------
+def Sinal():
+    url_temperatura = 'https://teste-esp-temp-default-rtdb.firebaseio.com/<Emanuelly>/Sensor/temperatura.json'
+    url_umidade = 'https://teste-esp-temp-default-rtdb.firebaseio.com/<Emanuelly>/Sensor/umidade.json'
+    
+    temperatura = float(requests.get(url_temperatura, proxies=proxies).content)
+    umidade = float(requests.get(url_umidade, proxies=proxies).content)
+    
+    return temperatura, umidade
+
+
+#Inserir dados -------------------------------------------------------------------------------------------
+def InserirBD(sinal):
+    server = 'CT-C-0013M\SQLEXPRESS'
+    database = 'tempdb'
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes')
+    global cursor
+    cursor = cnxn.cursor()
+    cursor.execute(f"INSERT dbo.Sensor (temp, umidade) VALUES ({sinal[0]},{sinal[1]});") 
+    cursor.commit()
+    print("Inserido com sucesso!")
+    
+    
+#Print py-------------------------------------------------------------------------------------------
+def Apresentar(sinal):
+    print(f"Temp: {sinal[0]}")
+    print(f"Temp: {sinal[1]}")
+
+    
+#Chama fun inserir bd a cada 2 min -------------------------------------------------------------------------------------------
+while True:
+    valores = Sinal()   
+    Apresentar(valores)
+    InserirBD(Sinal())
+    
+    cursor.execute('SELECT temp, umidade, timestamp FROM dbo.Sensor')
+    row = cursor.fetchone()
+    temperatura = []
+    umidade = []
+    timestamp = []
+
+    while row:
+        temperatura.append(row[0])
+        umidade.append(row[1])
+        timestamp.append(str(row[2]))
+        row = cursor.fetchone()
+            
+    df = pd.DataFrame({"Temperatura":temperatura, "Tempo":timestamp, "Umidade":umidade})
+    
+    df['Tempo'] = pd.to_datetime(df["Tempo"])
+    tempo = list(df['Tempo'])
+    hora_minuto = []
+    for i in tempo:
+        i = i.strftime('%H:%M')
+        hora_minuto.append(i)
+        
+    hora_minuto = pd.to_datetime(hora_minuto)
+    df['sla'] = hora_minuto
+    
+    sns.relplot(x= "sla", y="Temperatura", data=df, kind="line")
+    plt.title("Temperatura por Tempo")
+    plt.xticks(rotation=90)
+    plt.show()
+    time.sleep(120)
+    
+
+# Criar dtF-------------------------------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------------------------------
+
+
+#  url = 'https://random.dog/woof.json'
 # img_data = requests.get(url,proxies=proxies).content
 # new_url = json.loads(img_data)['url']
 # img = requests.get(new_url,proxies=proxies).content
 # with open('dog.jpg','wb') as dog_file:
 #     dog_file.write(img)
 
-# url_temperatura = 'https://teste-esp-temp-default-rtdb.firebaseio.com/<Emanuelly>/Sensor/temperatura.json'
-# url_umidade = 'https://teste-esp-temp-default-rtdb.firebaseio.com/<Emanuelly>/Sensor/umidade.json'
 
-# temperatura = float(requests.get(url_temperatura, proxies=proxies).content)
-# umidade = float(requests.get(url_umidade, proxies=proxies).content)
+# -------------------------------------------------------------------------------------------
 
+# Faça um programa utilizando a API https://agify.io/, faça um GET request enviando como
+# parâmetro o nome de uma pessoa, o dado retornado deve ser a previsão de idade para a
+# pessoa de acordo com seu nome.
 
-# print(temperatura, umidade)
+# name = input("Digite o nome da pessoa: ")
 
+# url = f"https://api.agify.io?name={name}&country_id=BR"
+# response = requests.get(url,proxies=proxies)
+# if response.status_code == 200:
+#     data = response.json()
+#     print(f"A idade prevista para {name} é {data['age']} anos.")
+# else:
+#     print(f"Erro ao realizar a requisição: {response.status_code}")
+    
+# -------------------------------------------------------------------------------------------
 
-name = input("Digite o nome da pessoa: ")
-
-url = f"https://api.agify.io?name={name}&country_id=BR"
-response = requests.get(url,proxies=proxies)
-if response.status_code == 200:
-    data = response.json()
-    print(f"A idade prevista para {name} é {data['age']} anos.")
-else:
-    print(f"Erro ao realizar a requisição: {response.status_code}")
     
     
--- teste/teste.ino
+++ teste/teste.ino
@@ -1,9 +1,10 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(19,23,18,17,16,15);
 
-#define led1 = 21
-#define led2 = 1
-#define led3 = 3
+const int led1 = 21;
+const int led2 = 1;
+const int led3 = 3;
+const int led4 = 5;
 
 #include "dht.h"
 const int pinoDHT = 22 ;
@@ -23,34 +24,40 @@ FirebaseData firebaseData;
 FirebaseJson json;
 
 void setup() {
+  
    pinMode(led1, OUTPUT);
    pinMode(led2, OUTPUT);
    pinMode(led3, OUTPUT);
+   pinMode(led4, OUTPUT);
+
+   lcd.begin(16,2);
+   
    DHT.read11(pinoDHT);
    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
    Serial.begin(9600);
-    while(WiFi.status() != WL_CONNECTED)
-    {
-      Serial.print(".");
-      delay(300);
-    }
-    Serial.print("Connected with IP: ");
-    Serial.println(WiFi.localIP());
+   while(WiFi.status() != WL_CONNECTED)
+   {
+     Serial.print(".");
+     delay(300);
+   }
+   Serial.print("Connected with IP: ");
+   Serial.println(WiFi.localIP());
 
     Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
     Firebase.reconnectWiFi(true);
     Firebase.setReadTimeout(firebaseData, 1000 * 60);
     Firebase.setwriteSizeLimit(firebaseData, "tiny");
-  
     Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-    
     Firebase.reconnectWiFi(true);
     Firebase.setReadTimeout(firebaseData, 1000 * 60);
     Firebase.setwriteSizeLimit(firebaseData, "tiny");
+    
     lcd.begin(16,2);
 }
+
 void loop() {
   DHT.read11(pinoDHT);
+  
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.println("Umidade: ");
@@ -58,21 +65,29 @@ void loop() {
   lcd.setCursor(0,1);
   lcd.print("Temperatura:");
   lcd.print(DHT.temperature, 0);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+
   if (DHT.temperature <= 25) {
       digitalWrite(led1, HIGH);
-      digitalWrite(led2, HIGH);
-      digitalWrite(led3, HIGH);
-
-    }
-  /*else if (t > 25 & t < 27) {
-      digitalWrite(pinos[1], HIGH);
-    }
-  else if (t <= 27) {
-      digitalWrite(pinos[1], LOW);
-      digitalWrite(pinos[2], HIGH);*/
   }
+  else if (DHT.temperature > 25 & DHT.temperature < 27) {
+      digitalWrite(led2, HIGH);
+  }
+  else if (DHT.temperature >= 27) {
+      digitalWrite(led3, HIGH);
+  }
+  else
+  {
+    digitalWrite(led4, HIGH);
+  }
+
+  
   json.set("/temperatura", DHT.temperature);
   json.set("/umidade", DHT.humidity);
   Firebase.updateNode(firebaseData, "/<Emanuelly>/Sensor", json);
-  delay(2000);
+  delay(3000);
 }  
